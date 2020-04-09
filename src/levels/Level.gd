1b3965a31bec0fa9f6c5c160a05a81dfb0e53351
Mastermori <mastermoriii@gmail.com>
2020-04-09 22:22:03 +0200

    added player spawn point

--- src/levels/Level.gd
+++ src/levels/Level.gd
@@ -4,8 +4,10 @@ class_name Level
 
 onready var canvas_layer := CanvasLayer.new()
 onready var game_objects := $GameObjects
+onready var spawn_pos := $GameObjects/PlayerSpawn
 
 func _ready():
 	add_child(canvas_layer)
 	Global.level = self
 	game_objects.add_child(Global.player)
+	Global.player.global_position = spawn_pos.global_position
